22c347d199cefe53291d7b68b23e8ed689e200bb
m.sinans <msomercikoglu@gmail.com>
2023-03-13 14:44:40 +0300

    Add project files.

---
+++ Console_Constructor_Yapici_Metot_3/Ogrenci.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Constructor_Yapici_Metot_3
+{
+    internal class Ogrenci
+    {
+        public Ogrenci(string bilgi)
+        {
+            Console.WriteLine("Durum: " + bilgi);
+        }
+    }
+}
---
+++ Console_Constructor_Yapici_Metot_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constructor_Yapici_Metot_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string blg;
+            Console.Write("Ad - Kulüp: ");
+            blg = Console.ReadLine();
+            Ogrenci ogr = new Ogrenci(blg);
+            Console.Read();
+        }
+    }
+}
+    
