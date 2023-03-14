0e67b7eb7baa9e619f8d4dc69cac7e1605102bbf
m.sinans <msomercikoglu@gmail.com>
2023-03-14 12:55:06 +0300

    14.03.2023

---
+++ Console_Constructor_Odev/KisiBilgileri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constructor_Odev
+{
+    internal class KisiBilgileri
+    {
+        public KisiBilgileri(string ad, string soyad, int yas, string meslek)
+        {
+            // Console.WriteLine("\n Adınız: " + ad + "\n " + "Soyadınız: " + soyad + "\n Yaşınız:  " +  yas + "\n Mesleğiniz: " + meslek);
+            Console.WriteLine(" ");
+            Console.WriteLine("Adınız: " + ad);
+            Console.WriteLine("Soyadınız: " + soyad);
+            Console.WriteLine("Yaşınız: " + yas);
+            Console.WriteLine("Mesleğiniz: " + meslek);
+        }
+    }
+}
---
+++ Console_Constructor_Odev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constructor_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.Write("Kisi adı: ");
+            ad = Console.ReadLine();
+
+            string soyad;
+            Console.Write("Kişi soyadı: ");
+            soyad = Console.ReadLine();
+
+            int yas;
+            Console.Write("Kişi yaşı: ");
+            yas = Convert.ToInt16(Console.ReadLine());
+
+            string meslek;
+            Console.Write("Kişi mesleği: ");
+            meslek = Console.ReadLine();
+
+            KisiBilgileri kisi = new KisiBilgileri(ad, soyad, yas, meslek);
+
+            
+
+        }
+    }
+}
